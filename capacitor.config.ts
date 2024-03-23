6b551f6756164e77b3f23c7ee1bc7db0cc3b399b
Angelo Padron <padron891@gmail.com>
2024-03-23 12:44:48 -0300

    Agregar archivos

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.changas',
+  appName: 'changas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
